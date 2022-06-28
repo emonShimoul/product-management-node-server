1dd5b7694b677f8ed091f72c928c8d3e29d4f86f
emonShimoul <emonshimoul@gmail.com>
2022-06-29 02:15:57 +0600

    use delete api to delete a specific product

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
-var cors = require('cors')
+const cors = require('cors')
+const ObjectId = require('mongodb').ObjectId;
 const port = process.env.PORT || 5000;
 
 app.use(cors());
@@ -26,6 +27,24 @@ async function run() {
             // console.log(product);
             const result = await productsCollection.insertOne(product);
             console.log("product inserted -", result);
+            res.json(result);
+        })
+
+        // GET API
+        app.get('/products', async(req, res) => {
+            const cursor = productsCollection.find({});
+            const productsDetails = await cursor.toArray(); 
+            res.json(productsDetails);
+        })
+
+        // DELETE API
+        app.delete('/products/:id', async(req, res) => {
+            const id = req.params.id;
+            // console.log(id);
+            const query = { _id:ObjectId(id) };
+            const result = await productsCollection.deleteOne(query);
+            console.log("deleted successfully", result);
+            res.json(result);
         })
     } finally {
         // await client.close();
